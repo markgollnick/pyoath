a9a7e203b41ad08dd52ebd51083d230b76d7cf12
Mark R. Gollnick <mark.r.gollnick@gmail.com>
2014-12-13 11:00:24 -0600

    Python >= 3.x support

--- test_builtins.py
+++ test_builtins.py
@@ -15,26 +15,26 @@ class HashLibSha1TestCase(TestCase):
     """
 
     def test_sha1_vector_1(self):
-        data = 'abc'
+        data = b'abc'
         expect = 'a9993e364706816aba3e25717850c26c9cd0d89d'
         result = hashlib.sha1(data).hexdigest()
         self.assertEqual(expect, result, 'Your SHA-1 implementation is broken')
 
     def test_sha1_vector_2(self):
-        data = 'abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq'
+        data = b'abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq'
         expect = '84983e441c3bd26ebaae4aa1f95129e5e54670f1'
         result = hashlib.sha1(data).hexdigest()
         self.assertEqual(expect, result, 'Your SHA-1 implementation is broken')
 
     def test_sha1_vector_3(self):
-        data = 'a' * 1000000
+        data = b'a' * 1000000
         expect = '34aa973cd4c4daa4f61eeb2bdbad27316534016f'
         result = hashlib.sha1(data).hexdigest()
         self.assertEqual(expect, result, 'Your SHA-1 implementation is broken')
 
     def test_sha1_vector_4(self):
-        data = ('01234567012345670123456701234567'
-                '01234567012345670123456701234567') * 10
+        data = (b'01234567012345670123456701234567'
+                b'01234567012345670123456701234567') * 10
         expect = 'dea356a2cddd90c7a7ecedc5ebb563934f460452'
         result = hashlib.sha1(data).hexdigest()
         self.assertEqual(expect, result, 'Your SHA-1 implementation is broken')
@@ -51,22 +51,22 @@ class HMACTestCase(TestCase):
     """
 
     def test_hmac_vector_1(self):
-        key = '\x0b' * 16
-        data = 'Hi There'
+        key = b'\x0b' * 16
+        data = b'Hi There'
         digest = '9294727a3638bb1c13f48ef8158bfc9d'
         result = hmac.new(key, data).hexdigest()
         self.assertEqual(digest, result, 'Your HMAC implementation is broken')
 
     def test_hmac_vector_2(self):
-        key = 'Jefe'
-        data = 'what do ya want for nothing?'
+        key = b'Jefe'
+        data = b'what do ya want for nothing?'
         digest = '750c783e6ab0b503eaa86e310a5db738'
         result = hmac.new(key, data).hexdigest()
         self.assertEqual(digest, result, 'Your HMAC implementation is broken')
 
     def test_hmac_vector_3(self):
-        key = '\xaa' * 16
-        data = '\xdd' * 50
+        key = b'\xaa' * 16
+        data = b'\xdd' * 50
         digest = '56be34521d144c88dbb8c733f0e8b3f6'
         result = hmac.new(key, data).hexdigest()
         self.assertEqual(digest, result, 'Your HMAC implementation is broken')
