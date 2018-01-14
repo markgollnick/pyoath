b22fd9600a1829675c4712935c0dead8d0cea5f6
Mark R. Gollnick <mark.r.gollnick@gmail.com>
2018-01-13 20:13:33 -0600

    Refactor tests: Use nose.tools instead of unittest

--- test_builtins.py
+++ test_builtins.py
@@ -1,72 +1,54 @@
 """Unit tests for Python's built-in hashlib and hmac modules."""
 
-from unittest import TestCase
-
 import hashlib  # Unit Under Test
 import hmac  # Unit Under Test
 
-
-class HashLibSha1TestCase(TestCase):
-
-    """
-    These tests are taken straight out of RFC 3174.
-
-    See Python's source code for more information.
-    """
-
-    def test_sha1_vector_1(self):
-        data = b'abc'
-        expect = 'a9993e364706816aba3e25717850c26c9cd0d89d'
-        result = hashlib.sha1(data).hexdigest()
-        self.assertEqual(expect, result, 'Your SHA-1 implementation is broken')
-
-    def test_sha1_vector_2(self):
-        data = b'abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq'
-        expect = '84983e441c3bd26ebaae4aa1f95129e5e54670f1'
-        result = hashlib.sha1(data).hexdigest()
-        self.assertEqual(expect, result, 'Your SHA-1 implementation is broken')
-
-    def test_sha1_vector_3(self):
-        data = b'a' * 1000000
-        expect = '34aa973cd4c4daa4f61eeb2bdbad27316534016f'
-        result = hashlib.sha1(data).hexdigest()
-        self.assertEqual(expect, result, 'Your SHA-1 implementation is broken')
-
-    def test_sha1_vector_4(self):
-        data = (b'01234567012345670123456701234567'
-                b'01234567012345670123456701234567') * 10
-        expect = 'dea356a2cddd90c7a7ecedc5ebb563934f460452'
-        result = hashlib.sha1(data).hexdigest()
-        self.assertEqual(expect, result, 'Your SHA-1 implementation is broken')
+from nose.tools import assert_equal
 
 
-class HMACTestCase(TestCase):
+def test_hashlib_sha1():
+    def test(supply, factor, expect):
+        actual = hashlib.sha1(supply * factor).hexdigest()
+        assert_equal(expect, actual)
 
-    """
-    These tests are taken straight out of RFC 2104.
+    # These tests are taken straight out of RFC 3174.
+    scenarios = [(
+        b'abc', 1,
+        'a9993e364706816aba3e25717850c26c9cd0d89d',
+    ), (
+        b'abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq', 1,
+        '84983e441c3bd26ebaae4aa1f95129e5e54670f1',
+    ), (
+        b'a', 1000000,
+        '34aa973cd4c4daa4f61eeb2bdbad27316534016f',
+    ), (
+        b'01234567012345670123456701234567', 20,
+        'dea356a2cddd90c7a7ecedc5ebb563934f460452',
+    )]
 
-    Python's hmac module also complies with the test vectors in RFC 4231.
+    for supply, factor, expect in scenarios:
+        yield test, supply, factor, expect
 
-    See Python's source code for more information.
-    """
 
-    def test_hmac_vector_1(self):
-        key = b'\x0b' * 16
-        data = b'Hi There'
-        digest = '9294727a3638bb1c13f48ef8158bfc9d'
-        result = hmac.new(key, data).hexdigest()
-        self.assertEqual(digest, result, 'Your HMAC implementation is broken')
+def test_hmac_md5():
+    def test(key, factor_1, msg, factor_2, expect):
+        actual = hmac.new(key * factor_1, msg * factor_2).hexdigest()
+        assert_equal(expect, actual)
 
-    def test_hmac_vector_2(self):
-        key = b'Jefe'
-        data = b'what do ya want for nothing?'
-        digest = '750c783e6ab0b503eaa86e310a5db738'
-        result = hmac.new(key, data).hexdigest()
-        self.assertEqual(digest, result, 'Your HMAC implementation is broken')
+    # These tests are taken straight out of RFC 2104.
+    scenarios = [(
+        b'\x0b', 16,
+        b'Hi There', 1,
+        '9294727a3638bb1c13f48ef8158bfc9d',
+    ), (
+        b'Jefe', 1,
+        b'what do ya want for nothing?', 1,
+        '750c783e6ab0b503eaa86e310a5db738',
+    ), (
+        b'\xaa', 16,
+        b'\xdd', 50,
+        '56be34521d144c88dbb8c733f0e8b3f6',
+    )]
 
-    def test_hmac_vector_3(self):
-        key = b'\xaa' * 16
-        data = b'\xdd' * 50
-        digest = '56be34521d144c88dbb8c733f0e8b3f6'
-        result = hmac.new(key, data).hexdigest()
-        self.assertEqual(digest, result, 'Your HMAC implementation is broken')
+    for key, factor_1, msg, factor_2, expect in scenarios:
+        yield test, key, factor_1, msg, factor_2, expect
--- test_pyoath.py
+++ test_pyoath.py
@@ -1,179 +1,140 @@
 """Unit tests for the pyoath module."""
 
+import codecs
+import hashlib
 import struct
-from unittest import TestCase
 
-from mock import patch, call
+from mock import patch
+from nose.tools import assert_equal
 
 import pyoath  # Unit Under Test
 
-try:
-    import builtins  # Python >= 3.x
-except ImportError:  # pragma: no cover
-    import __builtin__ as builtins  # Python < 3.x
 
-    class bytes(builtins.bytes):
-        def __new__(cls, x, encoding='ascii'):
-            return super(bytes, cls).__new__(cls, x.encode(encoding))
-
-        @classmethod
-        def fromhex(cls, x):
-            return x.decode('hex')
-
-
-class HMACSHA1TestCase(TestCase):
-
-    """
-    These tests are taken straight out of RFC 4226.
-
-    See that RFC document for more information.
-    """
-
-    def test_hmac_sha1_vectors_1_thru_10(self):
+def test_hmac_sha1():
+    def test(i, expect):
         secret = b'12345678901234567890'
-        expect = [
-            bytes.fromhex('cc93cf18508d94934c64b65d8ba7667fb7cde4b0'),
-            bytes.fromhex('75a48a19d4cbe100644e8ac1397eea747a2d33ab'),
-            bytes.fromhex('0bacb7fa082fef30782211938bc1c5e70416ff44'),
-            bytes.fromhex('66c28227d03a2d5529262ff016a1e6ef76557ece'),
-            bytes.fromhex('a904c900a64b35909874b33e61c5938a8e15ed1c'),
-            bytes.fromhex('a37e783d7b7233c083d4f62926c7a25f238d0316'),
-            bytes.fromhex('bc9cd28561042c83f219324d3c607256c03272ae'),
-            bytes.fromhex('a4fb960c0bc06e1eabb804e5b397cdc4b45596fa'),
-            bytes.fromhex('1b3c89f65e6c9e883012052823443f048b4332db'),
-            bytes.fromhex('1637409809a679dc698207310c8c7fc07290d9e5')]
-        for count in range(len(expect)):
-            result = pyoath._HMAC(secret, struct.pack('!Q', count))
-            self.assertEqual(expect[count], result)
+        count = struct.pack('>Q', i)[-8:]
+        actual = pyoath._HMAC(secret, count)
+        assert_equal(expect, actual)
+
+    # These tests are taken straight out of RFC 4226.
+    results = [
+        codecs.decode('cc93cf18508d94934c64b65d8ba7667fb7cde4b0', 'hex'),
+        codecs.decode('75a48a19d4cbe100644e8ac1397eea747a2d33ab', 'hex'),
+        codecs.decode('0bacb7fa082fef30782211938bc1c5e70416ff44', 'hex'),
+        codecs.decode('66c28227d03a2d5529262ff016a1e6ef76557ece', 'hex'),
+        codecs.decode('a904c900a64b35909874b33e61c5938a8e15ed1c', 'hex'),
+        codecs.decode('a37e783d7b7233c083d4f62926c7a25f238d0316', 'hex'),
+        codecs.decode('bc9cd28561042c83f219324d3c607256c03272ae', 'hex'),
+        codecs.decode('a4fb960c0bc06e1eabb804e5b397cdc4b45596fa', 'hex'),
+        codecs.decode('1b3c89f65e6c9e883012052823443f048b4332db', 'hex'),
+        codecs.decode('1637409809a679dc698207310c8c7fc07290d9e5', 'hex'),
+    ]
+
+    for i, expect in enumerate(results):
+        yield test, i, expect
 
 
-class DTTestCase(TestCase):
-
-    """
-    These tests are taken straight out of RFC 4226.
-
-    See that RFC document for more information.
-    """
-
-    def test_dynamic_truncation_vectors_1_thru_10(self):
+def test_DT():
+    def test(i, expect):
         secret = b'12345678901234567890'
-        expect = [
-            bytes.fromhex('4c93cf18'),
-            bytes.fromhex('41397eea'),
-            bytes.fromhex('082fef30'),
-            bytes.fromhex('66ef7655'),
-            bytes.fromhex('61c5938a'),
-            bytes.fromhex('33c083d4'),
-            bytes.fromhex('7256c032'),
-            bytes.fromhex('04e5b397'),
-            bytes.fromhex('2823443f'),
-            bytes.fromhex('2679dc69')]
-        for count in range(len(expect)):
-            hmac_sha1 = pyoath._HMAC(secret, struct.pack('!Q', count))
-            result = pyoath._DT(hmac_sha1)
-            self.assertEqual(expect[count], result)
+        count = struct.pack('>Q', i)[-8:]
+        hmac_sha1 = pyoath._HMAC(secret, count)
+        actual = pyoath._DT(hmac_sha1)
+        assert_equal(expect, actual)
+
+    # These tests are taken straight out of RFC 4226.
+    results = [
+        codecs.decode('4c93cf18', 'hex'),
+        codecs.decode('41397eea', 'hex'),
+        codecs.decode('082fef30', 'hex'),
+        codecs.decode('66ef7655', 'hex'),
+        codecs.decode('61c5938a', 'hex'),
+        codecs.decode('33c083d4', 'hex'),
+        codecs.decode('7256c032', 'hex'),
+        codecs.decode('04e5b397', 'hex'),
+        codecs.decode('2823443f', 'hex'),
+        codecs.decode('2679dc69', 'hex'),
+    ]
+
+    for i, expect in enumerate(results):
+        yield test, i, expect
 
 
-class HOTPTestCase(TestCase):
-
-    """
-    These tests are taken straight out of RFC 4226.
-
-    See that RFC document for more information.
-    """
-
-    def test_hotp_vectors_1_thru_10(self):
+def test_HOTP():
+    def test(count, expect):
         secret = b'12345678901234567890'
-        expect = [
-            '755224',
-            '287082',
-            '359152',
-            '969429',
-            '338314',
-            '254676',
-            '287922',
-            '162583',
-            '399871',
-            '520489']
-        for count in range(len(expect)):
-            result = pyoath.HOTP(secret, count)
-            self.assertEqual(expect[count], result)
+        actual = pyoath.HOTP(secret, count)
+        assert_equal(expect, actual)
+
+    # These tests are taken straight out of RFC 4226.
+    results = [
+        '755224',
+        '287082',
+        '359152',
+        '969429',
+        '338314',
+        '254676',
+        '287922',
+        '162583',
+        '399871',
+        '520489',
+    ]
+
+    for i, expect in enumerate(results):
+        yield test, i, expect
 
 
-class TOTPTestCase(TestCase):
+def test_TOTP():
+    @patch.object(pyoath, 'HOTP', wraps=pyoath.HOTP)
+    @patch.object(pyoath.time, 'time')
+    def test(unix_time, T, TOTP, Mode, mock_time, mock_HOTP):
+        mock_time.return_value = unix_time
 
-    """
-    These tests are taken straight out of RFC 6238.
+        # This is not noted in RFC 6238, but the secret changes with each Mode:
+        # 20 bytes for SHA-1, 32 bytes for SHA-256, and 64 bytes for SHA-512.
+        secret_len = len(Mode(b'x').digest())
 
-    See that RFC document for more information.
-    """
+        # The secret itself is just '0123456789',
+        # repeating for as many bytes as necesary.
+        secret = (b'1234567890' * 7)[:secret_len]
 
-    def setUp(self):
-        self.time = patch.object(pyoath.time, 'time').start()
-        self.time.side_effect = [
-            59,          # 1970-01-01 00:00:59 +0000
-            1111111109,  # 2005-03-18 01:58:29 +0000
-            1111111111,  # 2005-03-18 01:58:31 +0000
-            1234567890,  # 2009-02-13 23:31:30 +0000
-            2000000000,  # 2033-05-18 03:33:20 +0000
-            20000000000  # 2603-10-11 11:33:20 +0000
-        ]
+        result = pyoath.TOTP(secret, Digit=8, Mode=Mode)
+        mock_HOTP.assert_called_once_with(secret, T, 8, Mode)
+        assert_equal(TOTP, result)
 
-    def tearDown(self):
-        patch.stopall()
+    # These tests are taken straight out of RFC 6238.
+    scenarios = [
+        # 1970-01-01 00:00:59 Z
+        (59, int('0000000000000001', 16), '94287082', hashlib.sha1),
+        (59, int('0000000000000001', 16), '46119246', hashlib.sha256),
+        (59, int('0000000000000001', 16), '90693936', hashlib.sha512),
 
-    @patch.object(pyoath, 'HOTP')
-    def test_totp_time_vectors(self, hotp):
-        expect = [
-            int('0000000000000001', 16),
-            int('00000000023523ec', 16),
-            int('00000000023523ed', 16),
-            int('000000000273ef07', 16),
-            int('0000000003f940aa', 16),
-            int('0000000027bc86aa', 16)]
-        calls = []
-        for i in range(len(expect)):
-            pyoath.TOTP('secret')
-            calls.append(call('secret', expect[i], 6, pyoath.hashlib.sha1))
-        self.assertTrue(hotp.called)
-        self.assertEqual(len(expect), hotp.call_count)
-        hotp.assert_has_calls(calls)
+        # 2005-03-18 01:58:29 Z
+        (1111111109, int('00000000023523ec', 16), '07081804', hashlib.sha1),
+        (1111111109, int('00000000023523ec', 16), '68084774', hashlib.sha256),
+        (1111111109, int('00000000023523ec', 16), '25091201', hashlib.sha512),
 
-    def test_totp_sha1_vectors(self):
-        secret = b'1234567890' * 2  # 20 bytes
-        expect = [
-            '94287082',
-            '07081804',
-            '14050471',
-            '89005924',
-            '69279037',
-            '65353130']
-        for i in range(len(expect)):
-            result = pyoath.TOTP(secret, Digit=8)
-            self.assertEqual(expect[i], result)
+        # 2005-03-18 01:58:31 Z
+        (1111111111, int('00000000023523ed', 16), '14050471', hashlib.sha1),
+        (1111111111, int('00000000023523ed', 16), '67062674', hashlib.sha256),
+        (1111111111, int('00000000023523ed', 16), '99943326', hashlib.sha512),
 
-    def test_totp_sha256_vectors(self):
-        secret = (b'1234567890' * 3) + b'12'  # 32 bytes for SHA-256
-        expect = [
-            '46119246',
-            '68084774',
-            '67062674',
-            '91819424',
-            '90698825',
-            '77737706']
-        for i in range(len(expect)):
-            result = pyoath.TOTP(secret, Digit=8, Mode=pyoath.hashlib.sha256)
-            self.assertEqual(expect[i], result)
+        # 2009-02-13 23:31:30 Z
+        (1234567890, int('000000000273ef07', 16), '89005924', hashlib.sha1),
+        (1234567890, int('000000000273ef07', 16), '91819424', hashlib.sha256),
+        (1234567890, int('000000000273ef07', 16), '93441116', hashlib.sha512),
 
-    def test_totp_sha512_vectors(self):
-        secret = (b'1234567890' * 6) + b'1234'  # 64 bytes for SHA-512
-        expect = [
-            '90693936',
-            '25091201',
-            '99943326',
-            '93441116',
-            '38618901',
-            '47863826']
-        for i in range(len(expect)):
-            result = pyoath.TOTP(secret, Digit=8, Mode=pyoath.hashlib.sha512)
-            self.assertEqual(expect[i], result)
+        # 2033-05-18 03:33:20 Z
+        (2000000000, int('0000000003f940aa', 16), '69279037', hashlib.sha1),
+        (2000000000, int('0000000003f940aa', 16), '90698825', hashlib.sha256),
+        (2000000000, int('0000000003f940aa', 16), '38618901', hashlib.sha512),
+
+        # 2603-10-11 11:33:20 Z
+        (20000000000, int('0000000027bc86aa', 16), '65353130', hashlib.sha1),
+        (20000000000, int('0000000027bc86aa', 16), '77737706', hashlib.sha256),
+        (20000000000, int('0000000027bc86aa', 16), '47863826', hashlib.sha512),
+    ]
+
+    for unix_time, T, TOTP, Mode, in scenarios:
+        yield test, unix_time, T, TOTP, Mode
